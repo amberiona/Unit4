4086f548fb9228e6204f6386851980f18f5e7c76
amberiona <78923661+amberiona@users.noreply.github.com>
2021-06-24 15:41:03 +0100

    Add files via upload

---
+++ Unit 4 A2/CollegeApp/CollegeApp/UI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApp
+{
+    class UI
+    {
+        public void MainMenu()
+        {
+            string choice = "start";
+            while (!choice.Equals("quit"))
+            {
+                Console.WriteLine("");
+                Console.WriteLine("1 - Enter a new participant");
+                Console.WriteLine("2 - Enter a new team");
+                Console.WriteLine("3 - Enter a new event");
+                Console.WriteLine("4 - Enter a new tournamnet");
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("5 - Add participant to team");
+                Console.WriteLine("6 - Add team to event");
+                Console.WriteLine("7 - Add event to tournament");
+                Console.WriteLine("or type quit");
+                choice = Console.Readline();
+                switch (choice)
+
+                {
+                    case "1":
+                        Console.WriteLine("Enter name");
+                        string name = Console.ReadLine();
+                        Participant p = new Participant();
+                        p.setName(name);
+                        p.printParticipant();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter Team Name: ");
+                        string teamName = Console.ReadLine();
+                        Team t = new Team();
+                        t.setName(teamName);
+                        t.printTeam();
+                        allTeams.Add(t);
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Event Name:  ");
+                        string eventName = Console.ReadLine();
+                        Console.WriteLine("Enter Type: ");
+                        string eventType = Console.ReadLine();
+                        Events e = new Events();
+                        e.setName(eventName);
+                        e.setType(eventType);
+                        e.printEvent();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Enter Tournament Name: ");
+                        string tournamentName = Console.ReadLine();
+                        tournamnet o = new tournament();
+                        o.setName(tournamentName);
+                        o.printTournamnetName();
+                        o.printTournament();
+                        break;
+
+                    case "5":
+                        teamName teamSelected = allTeams[selectTeam()];
+                        Participant partSelected = allParticipants[selectParticipants()];
+                        teamSelected.addParticipant(partSelected);
+                        teamSelected.printTeam();
+                        partSelected.printParticipant();
+                        break;
+
+                    case "6":
+                        teamSelected = allTeam[selectTeam()];
+                        Events eventSelected = allEvents[selectEvent()];
+                        eventSelected.AddTeam(allTeams[selectTeam()]);
+                        eventSelected.printEvent();
+                        teamSelected.printTeam();
+                        break;
+
+                    case "7":
+                        eventSelected = allEvents[selectEvent()];
+                        tournament tournamnetSelected = allTournaments[selectTournament()];
+                        tournamentSelected.addEvent(allEvents[selectEvent()]);
+                        tournamnetSelected.printTournament();
+                        eventSelected.printEvent();
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Unit 4 A2/CollegeApp/CollegeApp/tournament.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApp
+{
+    class tournament
+    {
+        string tournamentname;
+        List<Events> events =new List<Events>();
+        List<Participant> participants = new List<Participant>();
+        List<Team> teams = new List<Team>();
+        string Participantname;
+
+        public void addEvent(Events e)
+        {
+            if (events.Count >= 5)
+            {
+                Console.WriteLine("Too many teams");
+            }
+            else
+            {
+                events.Add(e);
+            }
+        }
+
+        public void addParticipant(Participant p)
+        {
+            int numberOfParticipants = participants.Count;
+            if (numberOfParticipants == 20)
+            {
+                Console.WriteLine("There is no space for a new participant");
+            } else
+            {
+                Console.WriteLine("There are " + numberOfParticipants + " participants");
+                participants.Add(p);
+            }
+        }
+
+        public void addTeam(Team t)
+        {
+            int numberOfTeams = teams.Count;
+            if (numberOfTeams == 4)
+            {
+                Console.WriteLine("There is no space for a new team");
+            }
+            else
+            {
+                Console.WriteLine("There are " + numberOfTeams + " teams");
+                Teams.Add(t);
+            }
+        }
+
+        public void setName(string s)
+        {
+            this.eventname = s;
+        }
+
+        public string getEvent()
+        {
+            return eventname;
+        }
+
+        public void printTournament()
+        {
+            for (int i = 0; i < events.Count; i++)
+            {
+               // events[i].printEvent();
+            }
+        }
+    }
+
+}
